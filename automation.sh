f07d5a1f34d635b6babe01377d68c881f81f49c2
ShrutiMyIdeasMyBlogs <root@ip-172-31-8-109.ec2.internal>
2023-02-28 17:08:33 +0000

    second commit

--- automation.sh
+++ automation.sh
@@ -77,3 +77,33 @@ aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}
 
 
 
+
+
+######################## Checking if inventory file name is present,if not creating an inventory file name ######################
+if [ -e $inventory_file ]
+then
+    echo "inventory.html file exist"
+    file_size=$(ls -lh /tmp/${myname}-httpd-logs-${timestamp}.tar | awk '{ print $5}')
+    printf "<p>httpd-logs &emsp;&emsp;&emsp;&emsp; $timestamp &emsp;&emsp;&emsp;&emsp; tar &emsp;&emsp;&emsp;&emsp; $file_size \n" >> $inventory_file
+    echo "log file extractions are added to the inventory.html file"
+
+else
+    echo "inventory.html file doesnot exist,generating inventory.html file"
+    printf "<p style='padding: 10px; border: 2px solid #ccc; background-color:#f5f5f5;'>cat /var/www/html/inventory.html</p> \n <h3>Log Type &emsp;&emsp;&emsp; Date Created &emsp;&emsp;&emsp; Type &emsp;&emsp;&emsp; Size</h3> \n" > $inventory_file
+    file_size=$(ls -lh /tmp/${myname}-httpd-logs-${timestamp}.tar  | awk '{ print $5}')
+    printf "<p>httpd-logs &emsp;&emsp;&emsp;&emsp; $timestamp &emsp;&emsp;&emsp;&emsp; tar &emsp;&emsp;&emsp;&emsp; $file_size \n" >> $inventory_file
+    echo "log file extractions are added to the inventory.html file"
+fi
+#####################################################################################################################################
+
+
+############################################################# Creating a cron job ###################################################
+automate_cron_job="/etc/cron.d/automation"
+if [  -f $automate_cron_job ]
+then
+    echo "cron file for automation exist"
+else
+echo "cron job needs to be created,creating a cron job"
+    printf "0 0 * * * root /root/Automation_Project/automation.sh\n" > $automate_cron_job 
+fi
+#####################################################################################################################################
